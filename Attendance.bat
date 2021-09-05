b29cf9bfe269be600186adffe3509a99ce487795
kagurayayoi <862883646@qq.com>
2021-09-05 21:12:15 +0800

    Daily attendance

--- Attendance.bat
+++ Attendance.bat
@@ -4,6 +4,9 @@ echo === GitHub Daily Attendance ===
 rem Get:date --> mkdir:date
 
 md "%date:~2,2%%date:~5,2%%date:~8,2%"
+cd "%date:~2,2%%date:~5,2%%date:~8,2%"
+echo "# GitHub-Daily-Attendance" >> README.md
+cd ..
 
 rem Git:push --> Origin:main
 
